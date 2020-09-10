d015d981a9502a6fe81a1aa8e5657bd6a6794a32
Renée Cruz <reneebasscruz@gmail.com>
2020-09-09 22:20:44 -0400

    blog

--- spec/integration/blog_managment_spec.rb
+++ spec/integration/blog_managment_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe "blog_management" do 
     it "shows a list of a user's blogs" do 
         user = create(:user)
-
+  
         create_list(:blog, 10, :user => user)
 
         visit root_path
